29b8595e46119c613ad863356dfde11d9a35f606
josephpcoyne <joseph@coyne.dev>
2021-05-28 13:13:34 -0400

    :lipstick: Adds dropdown menu component

--- src/components/BaseList.vue
+++ src/components/BaseList.vue
@@ -1,8 +1,12 @@
 <template>
 	<!-- List component  -->
-	<div class="bg-gray-700 rounded-xl divide-y divide-gray-600">
+	<MenuItems as="div" class="bg-gray-700 rounded-xl divide-y divide-gray-600">
 		<!-- List Item  -->
-		<div class="flex flex-row justify-between items-center p-6">
+		<MenuItem
+			as="div"
+			v-slot="{ active }"
+			class="flex flex-row justify-between items-center p-6"
+		>
 			<!-- Item Content -->
 			<div class="flex flex-row space-x-4">
 				<!-- Item Icon -->
@@ -19,9 +23,13 @@
 					<h2 class="text-gray-400 text-sm">12 Games</h2>
 				</div>
 			</div>
-			<CheckIcon class="h-7 w-7 text-blue-500" />
-		</div>
-		<div class="flex flex-row justify-between items-center px-6 py-5">
+			<CheckIcon v-if="active" class="h-7 w-7 text-blue-500" />
+		</MenuItem>
+		<MenuItem
+			as="div"
+			v-slot="{ active }"
+			class="flex flex-row justify-between items-center px-6 py-5"
+		>
 			<div class="flex flex-row space-x-4">
 				<img
 					src="/img/crackhead.jpg"
@@ -34,18 +42,29 @@
 				</div>
 			</div>
 			<!-- Item secondary icon -->
-			<CheckIcon class="h-7 w-7 text-blue-500" />
-		</div>
+
+			<CheckIcon v-if="active" class="h-7 w-7 text-blue-500" />
+		</MenuItem>
 		<!-- List footer -->
 		<div class="flex flex-row space-x-2 items-center px-6 py-3">
 			<PlusCircleIcon class="h-5 w-5 text-gray-500" />
 			<h1 class="text-gray-300 leading-none">Add or join server</h1>
 		</div>
-	</div>
+	</MenuItems>
 </template>
 
 <script>
-export default {};
-</script>
+import { MenuItems, MenuItem } from '@headlessui/vue';
+import { CheckIcon, PlusCircleIcon } from '@heroicons/vue/solid';
 
-<style></style>
+export default {
+	name: 'BaseList',
+	components: { MenuItems, MenuItem, CheckIcon, PlusCircleIcon },
+	props: {
+		active: {
+			type: Boolean,
+			default: false,
+		},
+	},
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,47 @@
 <template>
-	<div class="bg-brand-black h-screen w-screen max-w-xs p-4">
-		<base-list></base-list>
+	<div class="bg-brand-black h-screen w-screen max-w-sm p-10">
+		<Menu>
+			<MenuButton>
+				<img
+					src="/img/crackhead.jpg"
+					alt="crackhead logo"
+					class="rounded-lg h-16 w-16 cursor-pointer"
+				/>
+			</MenuButton>
+			<base-list class="absolute w-80 mt-4"></base-list>
+		</Menu>
+
+		<ul class="mt-10">
+			<li class="flex flex-row items-center justify-between">
+				<span class="flex flex-row items-center space-x-1">
+					<!-- <LibraryIcon class="h-5 w-5 text-gray-500" /> -->
+					<h1 class="text-white leading-none">Library</h1>
+				</span>
+				<PlusCircleIcon class="h-5 w-5 text-gray-500" />
+			</li>
+		</ul>
 	</div>
 </template>
 
 <script>
+import { Menu, MenuButton, MenuItems, MenuItem } from '@headlessui/vue';
 import BaseList from '@/components/BaseList.vue';
-import { CheckIcon } from '@heroicons/vue/solid';
-import { PlusCircleIcon } from '@heroicons/vue/solid';
+import { PlusCircleIcon, LibraryIcon } from '@heroicons/vue/solid';
 
 export default {
+	data() {
+		return {
+			active: false,
+		};
+	},
 	components: {
+		Menu,
+		MenuButton,
+		MenuItems,
+		MenuItem,
 		BaseList,
-		CheckIcon,
 		PlusCircleIcon,
+		LibraryIcon,
 	},
 };
 </script>
